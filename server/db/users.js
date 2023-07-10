c39272f6090d6773123a185487bc70f1dc8b1f08
Ashot Israelyan <ashot@picsart.com>
2023-07-10 14:09:45 +0400

    Initial commit

---
+++ server/db/users.js
@@ -0,0 +1,11 @@
+import { connection } from './connection.js';
+
+const getUserTable = () => connection.table('user');
+
+export async function getUser(id) {
+  return getUserTable().first().where({ id });
+}
+
+export async function getUserByEmail(email) {
+  return getUserTable().first().where({ email });
+}
